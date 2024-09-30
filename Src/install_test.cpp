c2a02af5b31e19bddbd8aec0f74af16f19ba4230
shi-jia-liang <1301242876@qq.com>
2024-10-01 07:24:46 +0800

    The program environment has been configured, and CMake compilation has not been performed

--- Src/install_test.cpp
+++ Src/install_test.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 using namespace std;
 
 int main()
 {
-	cout << "hello world!"<<endl;
+	cv::Mat img;
+	cout << "hello world!" << endl;
+	img = cv::imread("D:/Projects/opencv4_test/Img/Lena_origin.png");
+	cv::imshow("test", img);
+	cv::waitKey(0);
 	return 0;
 }
- 
